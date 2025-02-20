37b1319672e4a96317a3cdc2ac9ca0adce68aa46
Joni Kemppainen <joni.kemppainen@windowslive.com>
2025-02-20 13:48:25 +0200

    Bump up version patch

--- src/devjoni/stllib/stllib.py
+++ src/devjoni/stllib/stllib.py
@@ -13,3 +13,5 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
+
+__version__ = "0.0.2"
